6084d3baa9946f4de8ac77c704b063ce779b1308
yonggyo1125 <yonggyo00@kakao.com>
2024-09-17 22:36:54 +0900

    프론트 예시 완료

--- src/commons/contexts/UserInfoContext.js
+++ src/commons/contexts/UserInfoContext.js
@@ -8,12 +8,16 @@ const UserInfoContext = createContext({
     // 상태 값
     userInfo: null,
     isLogin: false,
+    isCounselor: false,
+    isProfessor: false,
     isAdmin: false, // 관리자 여부
   },
   actions: {
     // 상태 변경 함수
     setUserInfo: null,
     setIsLogin: null,
+    setIsCounselor: null,
+    setIsProfessor: null,
     setIsAdmin: null,
   },
 });
@@ -21,11 +25,19 @@ const UserInfoContext = createContext({
 const UserInfoProvider = ({ children }) => {
   const [userInfo, setUserInfo] = useState(null);
   const [isLogin, setIsLogin] = useState(null);
+  const [isCounselor, setIsCounselor] = useState(null);
+  const [isProfessor, setIsProfessor] = useState(null);
   const [isAdmin, setIsAdmin] = useState(null);
 
   const value = {
-    states: { userInfo, isLogin, isAdmin },
-    actions: { setUserInfo, setIsLogin, setIsAdmin },
+    states: { userInfo, isLogin, isAdmin, isCounselor, isProfessor },
+    actions: {
+      setUserInfo,
+      setIsLogin,
+      setIsAdmin,
+      setIsCounselor,
+      setIsProfessor,
+    },
   };
 
   const token = cookies.load('token');
@@ -37,8 +49,9 @@ const UserInfoProvider = ({ children }) => {
         setUserInfo(user);
         setIsLogin(true);
 
-        const _isAdmin = user.authorities.some((a) => a.authority === 'ADMIN');
-        setIsAdmin(_isAdmin);
+        setIsAdmin(user.userType === 'ADMIN');
+        setIsCounselor(user.userType === 'COUNSELOR');
+        setIsProfessor(user.userType === 'PROFESSOR');
       } catch (err) {
         // 토큰 만료, 토큰이 잘못된 경우
         cookies.remove('token', { path: '/' });
