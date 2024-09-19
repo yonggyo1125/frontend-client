8f77fb3258a928cc27e5a3d89c9cd7cbdd58c783
yonggyo1125 <yonggyo00@kakao.com>
2024-09-19 11:18:44 +0900

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'next-app',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start',
+      args: 'start -p 7000',
       instances: 1,
       exec_mode: 'cluster',
     },
